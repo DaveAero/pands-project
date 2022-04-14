86d3ddf872bd356a58f4abcd0a1f1121e73acf4b
David Burke <davidgerburke@gmail.com>
2022-04-14 22:25:17 +0100

    loading data from iris.data

---
+++ project.py
@@ -0,0 +1,10 @@
+# project.py
+# Author: David Burke
+# Data analysis of the Fisher's Iris data set
+
+#improting libaries
+import pandas as pd
+
+#impoting of the data set
+irisData =  pd.read_csv('data\iris.data')
+print(irisData)
