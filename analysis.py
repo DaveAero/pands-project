4c71a4c64dcaa9d7099fb84e5e476f888f47507f
David Burke <davidgerburke@gmail.com>
2022-04-20 23:02:41 +0100

    Minor changes and code added to invesigate mean petal lenth vs SD of petal lenth

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 # Data analysis of the Fisher's Iris data set
 
 #improting libaries
+from statistics import mean, stdev
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,7 +19,7 @@ import matplotlib.pyplot as plt
 #      -- Iris Versicolour
 #      -- Iris Virginica
 irisData =  pd.read_csv('data\iris.data', names=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'FlowerClass'])
-#print(irisData.head())
+print(irisData.head())
 
 # Sererating Flowers into subsets of data for individual analysis
 irisSetosa = irisData[irisData['FlowerClass'] == "Iris-setosa"]
@@ -61,15 +62,15 @@ def variableHist(xAxis):
         plt.grid(axis = 'y')
 
     # Iris Setosa subplot
-    plt.subplot(3, 3, 7)
+    plt.subplot(2, 3, 4)
     subvariableHist(irisSetosa, xAxis, "cornflowerblue", "Iris Setosa")
 
     # Iris Versicolour subplot
-    plt.subplot(3, 3, 8)
+    plt.subplot(2, 3, 5)
     subvariableHist(irisVersicolour, xAxis, "mediumseagreen", "Iris Versicolour")
 
     # Iris Virginica subplot
-    plt.subplot(3, 3, 9)
+    plt.subplot(2, 3, 6)
     subvariableHist(irisVirginica, xAxis, "firebrick", "Iris Virginica")
 
     # outputting the histograms to a saved .png
@@ -103,15 +104,15 @@ def subplotter(xAxis, yAxis):
         plt.ylabel(yAxis, size = 9, color = 'black')
 
     # Iris Setosa subplot
-    plt.subplot(3, 3, 7)
+    plt.subplot(2, 3, 4)
     subplotterbot(irisSetosa, xAxis, yAxis, "cornflowerblue", "Iris Setosa")
 
     # Iris Versicolour subplot
-    plt.subplot(3, 3, 8)
+    plt.subplot(2, 3, 5)
     subplotterbot(irisVersicolour, xAxis, yAxis, "mediumseagreen", "Iris Versicolour")
 
     # Iris Virginica subplot
-    plt.subplot(3, 3, 9)
+    plt.subplot(2, 3, 6)
     subplotterbot(irisVirginica, xAxis, yAxis, "firebrick", "Iris Virginica")
 
     # Outputting the Scatter plots and saving as a .png
@@ -122,25 +123,25 @@ def subplotter(xAxis, yAxis):
 ###################### MAIN CODE ######################
 # 1. 
 # Creating a text summary for each variable
-#textOutput(irisData.describe(), "Summary of Iris Data set")
-#textOutput(irisSetosa.describe(), "Summary of Iris Setosa subset")
-#textOutput(irisVersicolour.describe(), "Summary of Iris Versicolour subset")
-#textOutput(irisVirginica.describe(), "Summary of Iris Virginica subset")
+textOutput(irisData.describe(), "Summary of Iris Data set")
+textOutput(irisSetosa.describe(), "Summary of Iris Setosa subset")
+textOutput(irisVersicolour.describe(), "Summary of Iris Versicolour subset")
+textOutput(irisVirginica.describe(), "Summary of Iris Virginica subset")
 
 # 2.
 # Creating histograms for each variable
-#for col in irisData.columns:
-#    variableHist(col)
+for col in irisData.columns:
+    variableHist(col)
 
 # 3.
 # Creating scatter potls for each pair of variables
-#subplotter("SepalLength", "SepalWidth")
+subplotter("SepalLength", "SepalWidth")
 
-#subplotter("PetalLength", "PetalWidth")
+subplotter("PetalLength", "PetalWidth")
 
-#subplotter("SepalLength", "PetalWidth")
+subplotter("SepalLength", "PetalWidth")
 
-#subplotter("PetalLength", "SepalWidth")
+subplotter("PetalLength", "SepalWidth")
 
 # 4. Extra
 # Analysis of the relationship between petal dimetions and the sepal relationships
@@ -158,11 +159,41 @@ irisVersicolourSepalRatio = irisVersicolour["SepalLength"] / irisVersicolour["Se
 irisVirginicaPetalRatio = irisVirginica["PetalLength"] / irisVirginica["PetalWidth"]
 irisVirginicaSepalRatio = irisVirginica["SepalLength"] / irisVirginica["SepalWidth"]
 
+z = np.polyfit(irisDataPetalRatio, irisDataSepalRatio, 1)
+p = np.poly1d(z)
+plt.plot(irisDataPetalRatio,p(irisDataPetalRatio), color= "black")
+
 plt.scatter(irisSetosaPetalRatio, irisSetosaSepalRatio, color="cornflowerblue")
+z = np.polyfit(irisSetosaPetalRatio, irisSetosaSepalRatio, 1)
+p = np.poly1d(z)
+plt.plot(irisSetosaPetalRatio,p(irisSetosaPetalRatio), color="cornflowerblue")
+
 plt.scatter(irisVersicolourPetalRatio, irisVersicolourSepalRatio, color= "mediumseagreen")
+z = np.polyfit(irisVersicolourPetalRatio, irisVersicolourSepalRatio, 1)
+p = np.poly1d(z)
+plt.plot(irisVersicolourPetalRatio,p(irisVersicolourPetalRatio), color="mediumseagreen")
+
 plt.scatter(irisVirginicaPetalRatio, irisVirginicaSepalRatio, color= "firebrick")
+z = np.polyfit(irisVirginicaPetalRatio, irisVirginicaSepalRatio, 1)
+p = np.poly1d(z)
+plt.plot(irisVirginicaPetalRatio,p(irisVirginicaPetalRatio), color="firebrick")
 
 plt.title("PetalRatio Vs SepalRatio", size = 20, color = 'black')
 plt.xlabel("PetalRatio", size = 12, color = 'black')
 plt.ylabel("SepalRatio", size = 12, color = 'black')
+plt.show()
+
+# 5. Extra
+# Analyis the relationship between the mean petal lenth and the standard deviation
+irisSetosaMean = mean(irisSetosa["PetalLength"])
+irisVersicolourMean = mean(irisVersicolour["PetalLength"])
+irisVirginicaMean = mean(irisVirginica["PetalLength"])
+
+irisSetosaSD = stdev(irisSetosa["PetalLength"])
+irisVersicolourSD = stdev(irisVersicolour["PetalLength"])
+irisVirginicaSD = stdev(irisVirginica["PetalLength"])
+
+plt.scatter(irisSetosaMean, irisSetosaSD, color="cornflowerblue")
+plt.scatter(irisVersicolourMean, irisVersicolourSD, color= "mediumseagreen")
+plt.scatter(irisVirginicaMean, irisVirginicaSD, color= "firebrick")
 plt.show()
